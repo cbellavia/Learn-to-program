4092e5e03bb84f9206d2cf2c809e713baed83dbb
Carmen Bellavia <carmen@bellaviacorp.com>
2013-05-07 00:44:02 -0400

    Chapter 3 examples

---
+++ chapter3.rb
@@ -0,0 +1,26 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts "###########"
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts 'blink ' * 4
+puts "###########"
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts "###########"
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+puts "###########"
+#puts '12' + 12
+#puts '2' * '5'
+
+puts 'You\'re Swell'
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
