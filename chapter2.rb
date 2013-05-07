9517c205c71682119af94ebdf632413fd2227bcf
Carmen Bellavia <carmen@bellaviacorp.com>
2013-05-07 00:22:31 -0400

    chapter 2 exercises

---
+++ chapter2.rb
@@ -0,0 +1,28 @@
+puts 1.0 + 2.0 /2 * 4
+puts 2.0 * 3.0 *100 * 2
+puts 5.0 - 8.0 + 40 /3
+puts 9.0 / 2.0
+puts '###############'
+
+puts 1 + 2 * 3.0
+puts 2 * 3 / (20 +16) + 4
+puts ((5 - 8) * (233 + 7)) /14
+puts 9 / 2 + 3.0 
+puts '###############'
+
+puts "Hours in a year"
+puts 365*24
+
+puts '###############'
+puts "Minutes in a decade"
+puts 10 * (365 + 2) * 24 * 60
+puts "or"
+puts 10 * (365 + 3) * 24 * 60
+
+puts '###############'
+puts "My age in seconds"
+puts 41 * (365 + 10) * 24 * 60 * 60
+
+puts '###############'
+puts "The author's age"
+puts 1111000000 / 60 / 60 / 24 / 365 
