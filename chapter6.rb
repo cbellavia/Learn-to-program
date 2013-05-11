8cd7dc232a823f5b985cf5d3314f7e3e63f9a4be
Carmen Bellavia <carmen@bellaviacorp.com>
2013-05-11 04:42:05 -0400

    Chapter 6

--- chapter6.rb
+++ chapter6.rb
@@ -1,3 +1,49 @@
-if __FILE__ == $0
-  # TODO Generated stub
-end
+#puts "WHAT DO YOU WANT!"
+#whatIwant = gets.chomp
+#puts "WHAT DO YOU MEAN #{whatIwant.upcase}?!?!  YOU'RE FIRED"
+
+#puts 5**2
+#puts 5**0.5
+#puts 7/3
+#puts 7%3
+#puts 365%7
+#puts (5-2).abs
+#puts (2-5).abs
+
+#puts rand
+#puts rand
+#puts rand
+#puts(rand(100))
+#puts(rand(100))
+#puts(rand(100))
+#puts(rand(1))
+#puts(rand(1))
+#puts(rand(1))
+#puts(rand(9999999999999999999999999999999999999))
+#puts "The weatherman said there is a #{rand(101)}% chance of rain, but you can never trust a weatherman."
+
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1977
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
